cc3aa23e36fe915d51eb0a63abb2beeb297c0c54
Lucien <Lucien.richer@ynov.com>
2023-02-14 09:41:51 +0100

    TP1->PWA

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newTodo, setNewTodo] = useState('')
+  const [ToDoList] = useState([])
+
+  const handleChange = (e) => {
+    setNewTodo(e.target.value)
+  }
+
+  const insert = () => {
+    ToDoList.push(newTodo)
+    setNewTodo('')
+  }
 
   return (
-    <div className="App">
+    <div>
+      <h1>To Do List</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="List">À Faire</label>
+        <div>
+        <input 
+          type="text" 
+          id="List" 
+          name="List"
+          value={newTodo}
+          onChange={handleChange}
+        />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => insert()}>Ajouter</button>
+      {ToDoList.map(e => <div>{e}</div>)}
+      
     </div>
   )
 }
 
-export default App
+export default App;
